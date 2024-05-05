f2d9b6fc90417263fe7c47e7bfe66271d3f4bac3
Cagatay Gedik <cagatay.gedik@icloud.com>
2024-05-05 13:47:12 +0300

    Created models file

---
+++ Animal Sounds/Models/ASAnimal.swift
@@ -0,0 +1,15 @@
+//
+//  Animal.swift
+//  Animal Sounds
+//
+//  Created by Celil Çağatay Gedik on 5.05.2024.
+//
+
+import Foundation
+
+struct ASAnimal {
+    let name: String
+    let imageName: String
+    let info: String
+    let soundFileName: String
+}
