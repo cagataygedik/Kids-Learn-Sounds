f742da2e23d8650f8a5bb9eb3a784abf2c2c29ff
Cagatay Gedik <cagatay.gedik@icloud.com>
2024-05-08 13:29:04 +0300

    Developed the Weather tab

--- Animal Sounds/Controllers/ViewControllers/Weather/KLSWeatherListViewController.swift
+++ Animal Sounds/Controllers/ViewControllers/Weather/KLSWeatherListViewController.swift
@@ -7,11 +7,49 @@
 
 import UIKit
 
-class KLSWeatherListViewController: UIViewController {
+final class KLSWeatherListViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    private var collectionView: UICollectionView!
+    private let weather = KLSWeatherData.weather.sorted { $0.name < $1.name }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureViewController()
+        configureCollectionView()
+    }
+    
+    private func configureViewController() {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        let removeAdsButton = UIBarButtonItem(title: "Remove Ads", style: .plain, target: self, action: #selector(removeAdsButtonTapped))
+        navigationItem.rightBarButtonItem = removeAdsButton
+    }
+    
+    @objc private func removeAdsButtonTapped() {
+        print("test")
+    }
+    
+    private func configureCollectionView() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
+        view.addSubview(collectionView)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemBackground
+        collectionView.register(KLSMainCell.self, forCellWithReuseIdentifier: KLSMainCell.reuseID)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return weather.count
+    }
+        
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: KLSMainCell.reuseID, for: indexPath) as! KLSMainCell
+        let weather = weather[indexPath.item]
+        cell.set(item: weather)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let selectedWeather = weather[indexPath.item]
+        SoundManager.shared.playSound(soundFileName: selectedWeather.soundFileName)
     }
 }
